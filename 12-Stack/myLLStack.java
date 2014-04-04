f74cb40a3ff3ef430a8750da6c37bd35714fd66d
calvinleenyc <calvinleenyc@gmail.com>
2014-04-04 15:38:26 -0400

    ?

---
+++ 12-Stack/myLLStack.java
@@ -0,0 +1,37 @@
+public class myLLStack{
+    private Node top;
+    private int length;
+    //constructor
+    public myLLStack(){
+	top = null;
+	length = 0;
+    }
+    public void push(String s){
+	Node n = new Node(s);
+	n.setNext(top);
+	top = n;
+	length++;
+    }
+    public String pop(){
+	String ans = top.getData();
+	top = top.getNext();
+	return ans;
+	length --;
+    }
+    public String peek(){
+	return top.getData();
+    }
+    public int size(){
+	return length;
+    }
+    public boolean isEmpty(){
+	return top == null;
+    }
+    public String toString(){
+	String ans = "";
+	for(Node temp = top; temp != null; temp = temp.getNext()){
+	    ans += temp.getData() + ", ";
+	}
+	return ans;
+    }
+}
